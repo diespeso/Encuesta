75dceedddb07189953cd71e343e789973bb57757
dgc1899 <daniel.garcia18@tectijuana.edu.mx>
2021-05-09 22:33:23 -0700

    Ventana de admin generica

---
+++ Encuesta/Menu-Admin.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace Encuesta
+{
+    partial class Menu_Admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu_Admin));
+            this.cmdNuevaEncuesta = new System.Windows.Forms.Button();
+            this.cmdModifEncuesta = new System.Windows.Forms.Button();
+            this.cmdElimEncuesta = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmdNuevaEncuesta
+            // 
+            this.cmdNuevaEncuesta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmdNuevaEncuesta.ForeColor = System.Drawing.SystemColors.Window;
+            this.cmdNuevaEncuesta.Image = ((System.Drawing.Image)(resources.GetObject("cmdNuevaEncuesta.Image")));
+            this.cmdNuevaEncuesta.Location = new System.Drawing.Point(12, 56);
+            this.cmdNuevaEncuesta.Name = "cmdNuevaEncuesta";
+            this.cmdNuevaEncuesta.Size = new System.Drawing.Size(162, 137);
+            this.cmdNuevaEncuesta.TabIndex = 0;
+            this.cmdNuevaEncuesta.UseVisualStyleBackColor = true;
+            // 
+            // cmdModifEncuesta
+            // 
+            this.cmdModifEncuesta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmdModifEncuesta.ForeColor = System.Drawing.SystemColors.Window;
+            this.cmdModifEncuesta.Image = ((System.Drawing.Image)(resources.GetObject("cmdModifEncuesta.Image")));
+            this.cmdModifEncuesta.Location = new System.Drawing.Point(318, 56);
+            this.cmdModifEncuesta.Name = "cmdModifEncuesta";
+            this.cmdModifEncuesta.Size = new System.Drawing.Size(162, 137);
+            this.cmdModifEncuesta.TabIndex = 1;
+            this.cmdModifEncuesta.UseVisualStyleBackColor = true;
+            // 
+            // cmdElimEncuesta
+            // 
+            this.cmdElimEncuesta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmdElimEncuesta.ForeColor = System.Drawing.SystemColors.Window;
+            this.cmdElimEncuesta.Image = ((System.Drawing.Image)(resources.GetObject("cmdElimEncuesta.Image")));
+            this.cmdElimEncuesta.Location = new System.Drawing.Point(626, 56);
+            this.cmdElimEncuesta.Name = "cmdElimEncuesta";
+            this.cmdElimEncuesta.Size = new System.Drawing.Size(162, 137);
+            this.cmdElimEncuesta.TabIndex = 2;
+            this.cmdElimEncuesta.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 261);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(162, 137);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(318, 261);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(162, 137);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(626, 261);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(162, 137);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Menu_Admin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.cmdElimEncuesta);
+            this.Controls.Add(this.cmdModifEncuesta);
+            this.Controls.Add(this.cmdNuevaEncuesta);
+            this.Name = "Menu_Admin";
+            this.Text = "Menu_Admin";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdNuevaEncuesta;
+        private System.Windows.Forms.Button cmdModifEncuesta;
+        private System.Windows.Forms.Button cmdElimEncuesta;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
