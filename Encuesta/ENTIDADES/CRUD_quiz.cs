72cf689a8a9bcd7531c0668ce204c2d49902692a
Carolina <carolina.dominguez18@tectijuana.edu.mx>
2021-05-10 20:08:35 -0700

    Creacion de drud de la entidad quiz

---
+++ Encuesta/ENTIDADES/CRUD_quiz.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encuesta.ENTIDADES
+{
+    class CRUD_quiz
+    {
+    }
+}
